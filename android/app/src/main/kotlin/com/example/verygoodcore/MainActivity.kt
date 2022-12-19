6046561a50891734ee11ab97417be4ba72ae424e
Saulo Souza <saulo.souza@mepoupenaweb.com>
2022-12-19 18:53:28 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.study_bloc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:study_bloc/app/app.dart';
+import 'package:study_bloc/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
